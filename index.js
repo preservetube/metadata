4ebafd61f953d37a2acb0178cf93d38c6eace25e
localhost <o0l2qp3e@duck.com>
2025-01-17 16:41:32 +0100

    make it more visable

--- index.js
+++ index.js
@@ -135,8 +135,8 @@ app.ws('/download/:id/:quality', async (ws, req) => {
 
     let videoTotal = videoFormat.content_length;
     if (videoTotal > (1_048_576 * 150)) {
-        ws.send('Is this content considered high risk? If so, please email me at admin@preservetube.com.')
-        ws.send('This video is too large, and unfortunately, Preservetube does not have unlimited storage.')
+        ws.send('<br/><b>Is this content considered high risk? If so, please email me at admin@preservetube.com.</b>');
+        ws.send('<b>This video is too large, and unfortunately, Preservetube does not have unlimited storage.</b><br/>');        
         return ws.close()
     }
     
